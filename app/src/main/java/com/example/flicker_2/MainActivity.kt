a155f9b4f8cde3497ad9cd04615a0cd69c99e0e2
linah_abdulaziz <linah.Abdulaziz@gmail.com>
2021-10-20 11:11:54 +0300

    Initial commit

---
+++ app/src/main/java/com/example/flicker_2/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.example.flicker_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainActivity : AppCompatActivity() {
+    var images = ArrayList<Photo>()
+    lateinit var recycle: RecyclerView
+    lateinit var search: EditText
+    lateinit var btnsearch: Button
+    lateinit var imgv: ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        recycle = findViewById(R.id.rv)
+        search = findViewById(R.id.edsearch)
+        imgv = findViewById(R.id.imageView)
+        btnsearch = findViewById(R.id.button)
+        recycle.adapter = RVAdapter(this, images)
+        recycle.layoutManager = LinearLayoutManager(this)
+        btnsearch.setOnClickListener {
+            Log.d("RESULT-Wrong", "hi")
+            if (edsearch.text.isNotEmpty()) {
+                RequestAPI()
+            } else {
+                Toast.makeText(applicationContext, "Search is empty", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        imgv.setOnClickListener { closeImg() }
+    }
+
+
+    private fun RequestAPI() {
+        val apiInterface = APIClinent().GetClient()?.create(APIInterface::class.java)
+        val call: Call<photoDetails?>? = apiInterface!!.doGetListResources(search.text.toString())
+        call?.enqueue(object : Callback<photoDetails?> {
+            override fun onResponse(
+                call: Call<photoDetails?>,
+                response: Response<photoDetails?>
+            ) {
+                for (photo in response.body()!!.photos.photo) {
+                    images.add(photo)
+                }
+
+
+                recycle.adapter?.notifyDataSetChanged()
+            }
+
+            override fun onFailure(call: Call<photoDetails?>, t: Throwable) {
+                call.cancel()
+            }
+
+        })
+    }
+
+        fun openImg(link: String){
+            Glide.with(this).load(link).into(imgv)
+            imgv.isVisible = true
+            recycle.isVisible = false
+            btnsearch.isVisible = false
+            search.isVisible = false
+        }
+
+        private fun closeImg(){
+            imgv.isVisible = false
+            recycle.isVisible = true
+            btnsearch.isVisible = true
+            search.isVisible = true
+        }
+    }
+
---
+++ app/src/main/java/com/example/flicker_2/photoDetails.kt
@@ -0,0 +1,20 @@
+package com.example.flicker_2
+
+import com.google.gson.annotations.SerializedName
+
+class photoDetails (val photos: Image, val stat: String) {
+
+//    @SerializedName("title")
+//    var : List<Datum>? = null
+//
+//    class Datum {
+//        @SerializedName("id")
+//        var id: Int? = null
+//
+//        @SerializedName("secret")
+//        var secret: String? = null
+//
+//        @SerializedName("server")
+//        var server: Int? = null
+
+}
